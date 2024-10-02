bd150961410f34d683c645698321957ff7b29972
zkf12 <nicolecurrens12@gmail.com>
2024-10-01 20:01:37 -0400

    Remove nltk import

--- bdr_code_challenge/brown_code_challenge/bdr_api/views.py
+++ bdr_code_challenge/brown_code_challenge/bdr_api/views.py
@@ -1,7 +1,6 @@
 import datetime, json, logging
 
 import trio
-import nltk
 from django.conf import settings as project_settings
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
